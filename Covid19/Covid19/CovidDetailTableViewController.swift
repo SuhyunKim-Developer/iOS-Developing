268d8494948dac846d9dbd5b94cb02afe02929e7
SueKim <julieish45@gmail.com>
2023-02-28 23:31:45 +1000

    Covid19 App Practice

---
+++ Covid19/Covid19/CovidDetailTableViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CovidDetailTableViewController.swift
+//  Covid19
+//
+//  Created by 수현 on 2023/02/28.
+//
+
+import UIKit
+
+class CovidDetailTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+}
